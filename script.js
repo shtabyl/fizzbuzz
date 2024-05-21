a2a86f001409d3ce490470ae2d2b1030dc36b9cf
shtabyl <avnirog@gmail.com>
2024-05-21 15:50:46 +0300

    Feature game logic

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 let button = document.querySelector('.section__button');
 let resultField = document.querySelector('.section__results');
 
+function fizzBuzz(n) {
+    for (let i = 1; i < n + 1; i++) {
+        let dividedByThree = i % 3;
+        let dividedByFive = i % 5;
+        if (dividedByThree && dividedByFive == 0) {
+            console.log('FizzBuzz');
+        } else {
+            if (dividedByThree == 0) {
+                console.log('Fizz');
+            } else if (dividedByFive == 0) {
+                console.log('Buzz');
+            } else {
+                console.log(i);
+            }
+        }
+    }
+}
